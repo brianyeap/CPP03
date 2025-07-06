e1919e654f2aeb9767097cc9a380baadd09ac0f3
Brian yeap <brianyeap@hotmail.com>
2025-07-06 19:19:24 +0800

    ex00

---
+++ ex00/src/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brian <brian@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/06 18:40:51 by brian             #+#    #+#             */
+/*   Updated: 2025/07/06 19:19:03 by brian            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "clapTrap.hpp"
+int main() {
+  std::cout << "\033[34mConstructing\033[0m" << std::endl;
+  ClapTrap a;
+  ClapTrap b("Cody");
+
+  std::cout << "\033[34mTesting\033[0m" << std::endl;
+  a.attack("some other robot");
+  a.takeDamage(10);
+  a.takeDamage(10);
+  a.beRepaired(5);
+  a.attack("some other other robot");
+  b.beRepaired(3);
+  for (int i = 0; i < 10; i++)
+    b.attack("Cody-clone");
+  b.beRepaired(3);
+  
+  std::cout << "\033[34mDeconstructing\033[0m" << std::endl;
+  return (0);
+}
